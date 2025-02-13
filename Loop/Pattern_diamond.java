6fb9fba7bc7841536e594d44e2529851e64fc6fc
harsh <harshgupt100@gmail.com>
2025-02-13 22:59:17 +0530

    pattern

---
+++ Loop/Pattern_diamond.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Pattern_diamond {
+    public static void main(String[] args) {
+        int a = 0;
+        
+        for(int r = 1 ; r <= 9 ; r++)
+        {
+            a = (r <= 5)?++a:--a;
+            for(int  c = 1 ; c <= 9 ; c++)
+            { 
+                if(c>=6-a && c <= 4+a)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        
+    }
+}

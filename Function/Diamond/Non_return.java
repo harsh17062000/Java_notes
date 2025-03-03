433302f04ba00bbd214ff9883dd121f57d90a099
harsh <harshgupt100@gmail.com>
2025-03-04 00:18:31 +0530

    diamond function

---
+++ Function/Diamond/Non_return.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Non_return {
+
+    static void Diamond()
+    {
+        
+    }
+    public static void main(String[] args) {
+        Diamond();
+    }
+}
